136a45b7fdd01ccd5289dc2e52744e92bdbfad70
EthanHarte <ethan.harte@yahoo.com>
2014-10-05 11:38:36 -0700

    yay

--- Arduino Programs/emocar/emocar.ino
+++ Arduino Programs/emocar/emocar.ino
@@ -10,6 +10,7 @@ const int BrkB = 8;
 char mode[3] = {'s', 's', 'x'};
 int leftSpeed = 0;
 int rightSpeed = 0;
+int i = 0;
 
 
 void setup()
@@ -35,64 +36,76 @@ void loop()
     mode[1] = Serial.read();
     mode[2] = Serial.read();
     
-    ///CASES
-    //first letter parsed
-    if (mode[0] == 'f') {
-      leftSpeed = 200;
-      rightSpeed = 200;
-      digitalWrite(DirA, HIGH);
-      digitalWrite(DirB, HIGH);
-    }
-    else if (mode[0] == 'b') {
-      leftSpeed = -250;
-      rightSpeed = -250;
-    }
-    else {
-      leftSpeed = 0;
-      rightSpeed = 0;
-    }
-    
-    //second and third letter parsed
-    if (mode[1] == 'l') {
-      if (mode[2] == 'l') {
-        leftSpeed -= 150;
-        rightSpeed += 150;
+    if (i == 0) {
+      ///CASES
+      //first letter parsed
+      if (mode[0] == 'f') {
+        leftSpeed = 200;
+        rightSpeed = 200;
+        digitalWrite(DirA, HIGH);
+        digitalWrite(DirB, HIGH);
       }
-      if (mode[2] == 'h') {
-        leftSpeed -= 250;
-        rightSpeed += 250;
+      else if (mode[0] == 'b') {
+        leftSpeed = -250;
+        rightSpeed = -250;
+      }
+      else {
+        leftSpeed = 0;
+        rightSpeed = 0;
+      }
+      
+      //second and third letter parsed
+      if (mode[1] == 'l') {
+        i = 40;
+        if (mode[2] == 'l') {
+          leftSpeed = 0;
+          rightSpeed += 250;
+        }
+        if (mode[2] == 'h') {
+          leftSpeed =0;
+          rightSpeed += 250;
+        }
+      }
+      else if (mode[1] == 'r') {
+        i = 40;
+        if (mode[2] == 'l') {
+          leftSpeed += 250;
+          rightSpeed =0;
+        }
+        if (mode[2] == 'h') {
+          leftSpeed += 250;
+          rightSpeed = 0;
+        }
+      }
+      
+      //Justify the speed values
+      if (leftSpeed > 255) {
+        leftSpeed = 255;
+      }
+      if (rightSpeed > 255) {
+        rightSpeed = 255;
+      }
+      
+      if (leftSpeed < 0) {
+        digitalWrite(DirB, LOW);
+        leftSpeed *= -1;
+      }
+      if (rightSpeed < 0) {
+        digitalWrite(DirA, LOW);
+        rightSpeed *= -1;
       }
     }
-    else if (mode[1] == 'r') {
-      if (mode[2] == 'l') {
-        leftSpeed += 150;
-        rightSpeed -= 150;
-      }
-      if (mode[2] == 'h') {
-        leftSpeed += 250;
-        rightSpeed -= 250;
-      }
-    }
-    
-    //Justify the speed values
-    if (leftSpeed > 255) {
-      leftSpeed = 255;
-    }
-    if (rightSpeed > 255) {
-      rightSpeed = 255;
-    }
-    
-    if (leftSpeed < 0) {
-      digitalWrite(DirB, LOW);
-      leftSpeed *= -1;
-    }
-    if (rightSpeed < 0) {
-      digitalWrite(DirA, LOW);
-      rightSpeed *= -1;
-    }
+
+  }
+  if (i > 0) {
+    i -= 1;
+  }
+  if(i == 20){
+    rightSpeed = 0;
+    leftSpeed = 0; 
   }
   //Actually write the pins
-  analogWrite(pwmA, rightSpeed);
-  analogWrite(pwmB, leftSpeed);
-  delay(40);
+  analogWrite(pwmA, rightSpeed* 0.855);
+  analogWrite(pwmB, leftSpeed );
+  delay(25);
 }
